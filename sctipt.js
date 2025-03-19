6e1c67df53abb51b38673d9ac9b67789a5404252
popeaura <popeahura@gmail.com>
2025-03-19 11:14:14 +0300

    Initial commit with security fixes

--- sctipt.js
+++ sctipt.js
@@ -1,19 +1,24 @@
-// Get DOM elements
 const bar = document.getElementById("bar")
 const close = document.getElementById("close")
 const nav = document.getElementById("navbar")
 
-// Toggle menu open when hamburger icon is clicked
+
 if (bar) {
   bar.addEventListener("click", () => {
     nav.classList.add("active")
+
+    if (close) {
+      close.style.display = "block"
+    }
   })
 }
 
-// Toggle menu closed when close icon is clicked
+
 if (close) {
   close.addEventListener("click", () => {
     nav.classList.remove("active")
+
+    close.style.display = "none"
   })
 }
 
