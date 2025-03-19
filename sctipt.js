b1a57f6b0fa57bcfe7d01fb9b3186bf919083cb8
popeaura <popeahura@gmail.com>
2025-03-19 10:51:50 +0300

    Initial commit with security fixes

--- sctipt.js
+++ sctipt.js
@@ -1,8 +1,17 @@
-const bar = document.getElementById('bar');
-const nav = document.getElementById('navbar');
+document.addEventListener("DOMContentLoaded", () => {
+    const bar = document.getElementById("bar");
+    const closeBtn = document.getElementById("close");
+    const navbar = document.getElementById("navbar");
 
-if (bar){
-    bar.addEventListener('click', () =>{
-        nav.classList.add('active')
-    })
-}
+    if (bar) {
+        bar.addEventListener("click", () => {
+            navbar.classList.add("active");
+        });
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener("click", () => {
+            navbar.classList.remove("active");
+        });
+    }
+});
